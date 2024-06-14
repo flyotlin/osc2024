026ba13d0e433748bf2a4eeac1e72100088114df
flyotlin <flyotlin@gmail.com>
2024-06-14 14:29:56 +0800

    Enlarge kmalloc heap size

--- src/malloc.h
+++ src/malloc.h
@@ -3,7 +3,7 @@
 
 #define NULL         ((void *)0)
 #define HEAP_START   0x2000000      // 0x10 breaks on rpi3b+
-#define HEAP_SIZE    0x10000
+#define HEAP_SIZE    0x50000000
 
 // kernel malloc
 void *kmalloc(unsigned int);
